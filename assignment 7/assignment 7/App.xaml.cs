6042a07f9a2a7eda8f701bac9971f42b4038fe85
amani-bit-dot <maniandiswa1@gmail.com>
2020-11-19 02:40:25 +0200

    Initial commit

---
+++ assignment 7/assignment 7/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace assignment_7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            if (DesignMode.IsDesignModeEnabled)
+            {
+                return;
+            }
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
